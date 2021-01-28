4e9bd91919a3207dfe1e757e811c552cd8e95894
Fabian Hernandez <h.fabian25f@gmail.com>
2021-01-27 23:02:36 -0600

    Added first eror

--- src/playground1/Main.java
+++ src/playground1/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		//System.out.print(notexisting);
+		//Error: Unresolved compilation problem: 
+		//	notexisting cannot be resolved to a variable
+
 
 	}
 
